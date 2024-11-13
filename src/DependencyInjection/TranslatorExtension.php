2da54f28226d4998fe711b54b0c913627b27d286
Vladimir Danilov <dev.danilovl@gmail.com>
2024-11-13 20:09:07 +0100

    Composer update.

--- src/DependencyInjection/TranslatorExtension.php
+++ src/DependencyInjection/TranslatorExtension.php
@@ -6,7 +6,7 @@ use Danilovl\TranslatorBundle\Util\TranslatorConfigurationUtil;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 
 class TranslatorExtension extends Extension
 {
